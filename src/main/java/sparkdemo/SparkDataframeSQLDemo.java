1eb0b6565179ac5a844c3e85eea24cefd201dbfc
Frank Callaly <frankcallaly@gmail.com>
2022-02-03 16:05:53 +0000

    Add write

--- src/main/java/sparkdemo/SparkDataframeSQLDemo.java
+++ src/main/java/sparkdemo/SparkDataframeSQLDemo.java
@@ -2,6 +2,7 @@ package sparkdemo;
 
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.SparkSession;
 
 import java.util.Arrays;
@@ -47,10 +48,27 @@ public class SparkDataframeSQLDemo {
 
 
         // What did we just read in?
+        countries.show();
         // - check the schema - display the first few rows
+        countries.printSchema();
 
         // Experiment with filtering - e.g. which country has the greatest LifeExpectancy?
 
+
+
         // What's the average GNP per Region?
+        Dataset<Row> regionalGnp = countries.groupBy("Region").avg("GNP");
+        regionalGnp.show();
+
+        Properties jdbcProps = new Properties();
+        jdbcProps.put("password", password);
+        regionalGnp.write()
+                .mode(SaveMode.ErrorIfExists)
+                .option("driver", "com.mysql.cj.jdbc.Driver")
+                .option("url", jdbcUrl)
+                .option("username", username)
+                .option("password", password)
+                .jdbc(jdbcUrl, "YYYYRegionalGnp", jdbcProps);
+
     }
 }
